970748ce1d46ea23d1285d7e7d90d04c87500032
Levi Liester <levi.liester@gmail.com>
2021-08-29 16:40:27 -0500

    Adding static value for /jobs response

--- server/server.js
+++ server/server.js
@@ -9,24 +9,36 @@ const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
 app.get('/jobs', async (req, res) => {
-  try {
-    let { description = '', full_time, location = '', page = 1 } = req.query;
-
-    description = description ? encodeURIComponent(description) : '';
-    location = location ? encodeURIComponent(location) : '';
-    full_time = full_time === 'true' ? '&full_time=true' : '';
-    if (page) {
-      page = parseInt(page);
-      page = isNaN(page) ? '' : `&page=${page}`;
+    try {
+        // let { description = '', full_time, location = '', page = 1 } = req.query;
+        //
+        // description = description ? encodeURIComponent(description) : '';
+        // location = location ? encodeURIComponent(location) : '';
+        // full_time = full_time === 'true' ? '&full_time=true' : '';
+        // if (page) {
+        //   page = parseInt(page);
+        //   page = isNaN(page) ? '' : `&page=${page}`;
+        // }
+        // const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${full_time}${page}`;
+        // const result = await axios.get(query);
+        const jobs = [
+            {
+                id: 1,
+                type: "Awesome",
+                created_at: new Date(),
+                company: "Mind Canary",
+                location: "Montana",
+                title: "Extra Most Bestest",
+                company_logo: "https://dogtime.com/assets/uploads/2016/08/corgi-puppy-6-e1573588370274.jpg",
+                index: 99
+            }
+        ]
+        res.send(jobs);
+    } catch (error) {
+        res.status(400).send('Error while getting list of jobs.Try again later.');
     }
-    const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${full_time}${page}`;
-    const result = await axios.get(query);
-    res.send(result.data);
-  } catch (error) {
-    res.status(400).send('Error while getting list of jobs.Try again later.');
-  }
 });
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+    console.log(`server started on port ${PORT}`);
 });
